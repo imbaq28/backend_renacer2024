cd6b896f4262304bd28d54b1b1c367bbd11c6cea
pepesossa <jfgosugank@gmail.com>
2024-05-27 11:41:03 -0400

    27 de mayo y todo el backend hasta productos/medicamentos

--- src/common/config/db.js
+++ src/common/config/db.js
@@ -1,7 +1,7 @@
 const db = {
-  database: process.env.DB_NAME || "demo",
+  database: process.env.DB_NAME || "renacerDB",
   username: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASS || "root",
+  password: process.env.DB_PASS || "oeoeoe",
   host: process.env.DB_HOST || "localhost",
   dialect: "postgres",
   timezone: "America/La_Paz",
